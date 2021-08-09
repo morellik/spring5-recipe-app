5c263869aed917e86bfb46554499bcd59b555537
morelli <morelli@cerm.unifi.it>
2021-08-09 12:11:52 +0200

    Added Category model and its relationship

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -27,6 +27,11 @@ public class Recipe {
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+    joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     @Lob // To avoid 255 characters limitation
     private Byte[] image;
 
@@ -51,6 +56,14 @@ public class Recipe {
 
     public Integer getPrepTime() {
         return prepTime;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 
     public void setPrepTime(Integer prepTime) {
